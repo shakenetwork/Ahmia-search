b783e28b32d06c3e08d9bc644cc040543175b48b
juhanurmi <juha.nurmi@ahmia.fi>
2015-12-30 07:28:17 +0200

    onionElasticBot

--- onionElasticBot/onionElasticBot/spiders/i2pspider.py
+++ onionElasticBot/onionElasticBot/spiders/i2pspider.py
@@ -2,27 +2,47 @@
 
 import re
 from urlparse import urlparse
-
+import os
 import html2text
-import scrapy
 from scrapy.conf import settings
+
+from onionElasticBot.items import CrawledWebsiteItem
 from scrapy.contrib.linkextractors import LinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.selector import HtmlXPathSelector
-
-from onionMongoBot.items import CrawledWebsiteItem
+from scrapy.selector import Selector
 
 
-class OnionSpider(CrawlSpider):
-    name = "OnionSpider"
-    allowed_domains = ["onion"]
-    start_urls = settings.get('TARGET_SITES')
+class i2pSpider(CrawlSpider):
+    name = "i2pSpider"
+    ALLOWED_DOMAINS = settings.get('ALLOWED_DOMAINS')
+
+    if ALLOWED_DOMAINS and os.path.isfile(ALLOWED_DOMAINS):
+        # Read a list of URLs from file
+        # Create the target file list
+        with open(ALLOWED_DOMAINS) as f:
+            allowed_domains = f.read().splitlines() # Make it to Python list
+            allowed_domains = filter(None, allowed_domains) # Remove empty strings
+    else:
+        allowed_domains = ["i2p"]
+
+    TARGET_SITES = settings.get('TARGET_SITES')
+
+    if TARGET_SITES and os.path.isfile(TARGET_SITES):
+        # Read a list of URLs from file
+        # Create the target file list
+        with open(TARGET_SITES) as f:
+            start_urls = f.read().splitlines() # Make it to Python list
+            start_urls = filter(None, start_urls) # Remove empty strings
+    else:
+        start_urls = [ 'http://nekhbet.com/i2p_links.shtml', ]
 
     rules = (Rule(LinkExtractor(), callback='parse_item', follow=True), )
 
     def parse_item(self, response):
-        hxs = HtmlXPathSelector(response)
+        hxs = Selector(response)
         item = CrawledWebsiteItem()
+        # Also the header
+        item['header'] = response.headers
         item['url'] = response.url
         # Add the domain
         domain = urlparse( item['url'] ).hostname
@@ -36,10 +56,24 @@ class OnionSpider(CrawlSpider):
         item['title'] = title
         encoding = self.detect_encoding(response)
         decoded_html = response.body.decode(encoding, 'ignore')
-        item['html'] = decoded_html
         html_text = self.html2string(decoded_html)
         words = self.extract_words(html_text)
-        item['words'] = title + " " + " ".join(words)
+        item['text'] = title + " " + " ".join(words)
+        # For each link on this page
+        item['links'] = []
+        links = hxs.xpath('//a')
+        for link in links:
+            link_obj = {}
+            # Extract the link's URL
+            link_str = " ".join(link.xpath('@href').extract())
+            link_obj['link'] = link_str.replace("\n", "")
+            # Extract the links value
+            link_name_str = " ".join(link.xpath('text()').extract())
+            link_name_str = link_name_str.replace("\n", "")
+            link_name_str = link_name_str.lstrip()
+            link_name_str = link_name_str.rstrip()
+            link_obj['link_name'] = link_name_str
+            item['links'].append(link_obj)
         return item
 
     def detect_encoding(self, response):
@@ -61,7 +95,7 @@ class OnionSpider(CrawlSpider):
         words = []
         for word in string_list:
             # Word must be longer than 0 letter
-            # And shorter than 45
+            # And shorter than 45sudo service polipo restart
             # The longest word in a major English dictionary is
             # Pneumonoultramicroscopicsilicovolcanoconiosis (45 letters)
             if len(word) > 0 and len(word) <= 45:
