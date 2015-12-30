ffafb23e2728222577e8ea63bce21b03505e36a3
root <root@ahmia.fi>
2015-12-30 13:21:56 +0100

    New settings to production mode.

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append('/usr/local/lib/ahmia')
+sys.path.append('/usr/local/lib/search')
 
 # Path to the settings.py file
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ahmia.settings")
